d66a3a170e6cecf47b48ef38654cd7d3b88ce0b2
Steve <YOUR_GITHUB_EMAIL>
2026-05-08 14:18:41 -0400

    rm -rf .next rm -rf node_modules/.cache

--- lafflab-frontend/src/app/creator/analytics/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CreatorAnalyticsPage() {
-  return (
-    <div className="p-4 text-white">
-      <h1 className="text-xl font-semibold mb-4">Analytics</h1>
-      <p className="text-white/60">Analytics coming soon.</p>
-    </div>
-  );
-}
