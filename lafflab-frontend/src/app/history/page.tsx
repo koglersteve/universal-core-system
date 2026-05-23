9772da5e95d589e55ce4d01bf5427f614ffab0fb
Steve <YOUR_GITHUB_EMAIL>
2026-05-22 22:09:29 -0400

    git add . git commit -m "Remove legacy history system and all related files" git push

--- lafflab-frontend/src/app/history/page.tsx
+++
@@ -1,9 +0,0 @@
-import HistoryList from "@/components/history/HistoryList";
-
-export default function HistoryPage() {
-  return (
-    <div className="p-4">
-      <HistoryList />
-    </div>
-  );
-}
