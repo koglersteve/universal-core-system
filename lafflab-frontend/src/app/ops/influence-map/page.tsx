3d8bb86db12dd7ca9ba729bd6fa9664c1cbc8b48
Steve <YOUR_GITHUB_EMAIL>
2026-05-24 15:55:07 -0400

    git commit -m "Remove obsolete explore, ops, profile/posts, and search routes referencing deleted components"

--- lafflab-frontend/src/app/ops/influence-map/page.tsx
+++
@@ -1,9 +0,0 @@
-import InfluenceMap from "@/components/ops/InfluenceMap";
-
-export default function InfluenceMapPage() {
-  return (
-    <div className="p-4">
-      <InfluenceMap />
-    </div>
-  );
-}
